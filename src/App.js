274a39b2794c772cd66a4b94b2d0658cb78872f5
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2023-10-23 19:51:12 +0700

    RouteProvider, BrowserRouter, Outlet, Nested layout & route

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { 
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
+
+//pages
+import Home from './pages/Home'
+import About from './pages/About'
+
+//layouts
+import RootLayout from './layouts/RootLayout';
+import HelpLayout from './layouts/HelpLayout';
+import Faq from './pages/help/FAQ';
+import Contact from './pages/help/Contact';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout /> }>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='help' element={<HelpLayout />}>
+        <Route path='faq' element={<Faq />} />
+        <Route path='contact' element={<Contact />} />
+      </Route>
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    
+    <RouterProvider router={router}/>
+    
+  );
+}
+
+export default App;
