46fa72184eccaf32a1efa21bfa76b23aa4107d52
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2024-11-27 21:17:26 +0700

    change domain

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {
-  createBrowserRouter,
+  createHashRouter,
   Route,
   createRoutesFromElements,
   RouterProvider,
@@ -12,7 +12,9 @@ import Faq from "./pages/help/FAQ";
 import Contact, { contactAction } from "./pages/help/Contact";
 import NotFound from "./pages/NotFound";
 import Careers, { careersLoader } from "./pages/careers/Careers";
-import CareersDetails, { careerDetailsLoader } from "./pages/careers/CareersDetails";
+import CareersDetails, {
+  careerDetailsLoader,
+} from "./pages/careers/CareersDetails";
 import CareersError from "./pages/careers/CareersError";
 
 //layouts
@@ -20,7 +22,7 @@ import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
 import CareersLayout from "./layouts/CareersLayout";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
@@ -28,18 +30,15 @@ const router = createBrowserRouter(
 
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact />} action={contactAction}/>
+        <Route path="contact" element={<Contact />} action={contactAction} />
       </Route>
 
-      <Route 
-        path="careers" 
-        element={<CareersLayout />} 
-        errorElement={<CareersError />}>
-        <Route 
-          index 
-          element={<Careers />}  
-          loader={careersLoader}          
-        />
+      <Route
+        path="careers"
+        element={<CareersLayout />}
+        errorElement={<CareersError />}
+      >
+        <Route index element={<Careers />} loader={careersLoader} />
         <Route
           path=":id"
           element={<CareersDetails />}
